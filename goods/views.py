48b0482aa9effbd1f2f25bc2d0f9adb375e90a81
Grigory Malanchuk <grigorymalanchuk@gmail.com>
2024-07-09 20:09:49 +0300

    кеш

--- goods/views.py
+++ goods/views.py
@@ -1,5 +1,12 @@
+import json
 import logging
+from urllib.parse import urlencode
 
+from django.core.cache import cache
+from django.core.serializers.json import DjangoJSONEncoder
+from django.http import JsonResponse
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 from django_filters.rest_framework import DjangoFilterBackend
 from drf_spectacular.utils import extend_schema
 from rest_framework.exceptions import PermissionDenied
@@ -45,6 +52,20 @@ class PackageViewSet(CreateModelMixin, ListModelMixin, RetrieveModelMixin, Gener
         Также возможна фильтрация по типу посылки и факту наличия
         рассчитанной стоимости доставки"""
 
+        # TODO вынести кеш в отдельный метод
+        # Создание cache_key с учётом параметров запроса
+        sorted_query_params = sorted(request.GET.items())
+        query_string = urlencode(sorted_query_params, doseq=True)
+
+        session_key = request.session.session_key
+        cache_key = f"packages_list_{session_key}_{query_string}"
+        cached_response = cache.get(cache_key)
+
+        # Если ответ на запрос уже кеширован, то возвращаем его
+        if cached_response:
+            logger.info("Package list fetched from cache")
+            return JsonResponse(json.loads(cached_response), safe=False)
+
         articles = request.session.get('articles', [])
 
         if not articles:
@@ -56,11 +77,24 @@ class PackageViewSet(CreateModelMixin, ListModelMixin, RetrieveModelMixin, Gener
         response = super().list(request, *args, **kwargs)
         logger.info("Package list fetched successfully")
 
+        cache.set(cache_key, json.dumps(response.data, cls=DjangoJSONEncoder), timeout=60*5)
+
         return response
 
     def retrieve(self, request, *args, **kwargs):
         """Получение информации о посылке по её ID.
         Пользователь может просматривать только свои посылки"""
+
+        # TODO вынести кеш в отдельный метод
+        package_id = kwargs.get('pk', None)
+        session_key = request.session.session_key
+        cache_key = f"package_{package_id}_{session_key}"
+        cached_response = cache.get(cache_key)
+
+        # Если ответ на запрос уже кеширован, то возвращаем его
+        if cached_response:
+            logger.info(f"Package with article {cached_response['article']} fetched from cache")
+            return JsonResponse(cached_response, safe=False)
 
         response = super().retrieve(request, *args, **kwargs)
 
@@ -70,6 +104,8 @@ class PackageViewSet(CreateModelMixin, ListModelMixin, RetrieveModelMixin, Gener
             raise PermissionDenied
 
         logger.info(f"Package with article {response.data['article']} successfully retrieved")
+        cache.set(cache_key, response.data, timeout=60 * 5)
+
         return response
 
     def get_serializer_class(self):
@@ -79,6 +115,7 @@ class PackageViewSet(CreateModelMixin, ListModelMixin, RetrieveModelMixin, Gener
 
 
 @extend_schema(tags=['Types'])
+@method_decorator(cache_page(60*5), name='dispatch')
 class TypeListAPIView(ListAPIView):
     queryset = Type.objects.all()
     serializer_class = TypeSerializer
